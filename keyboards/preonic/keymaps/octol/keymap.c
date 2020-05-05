ea8174e2ce0562c376b50d9689c14589e17b7d6b
Jon Häggblad <jon.haggblad@gmail.com>
2020-05-05 10:08:26 +0200

    move rgui to the right

--- keyboards/preonic/keymaps/octol/keymap.c
+++ keyboards/preonic/keymaps/octol/keymap.c
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,          KC_Q,  KC_W,    KC_E,            KC_R,  KC_T,           KC_Y,           KC_U,    KC_I,           KC_O,    KC_P,    KC_BSPC, \
       LCTL_T(KC_ESC),  KC_A,  KC_S,    KC_D,            KC_F,  KC_G,           KC_H,           KC_J,    KC_K,           KC_L,    KC_SCLN, KC_ENT, \
       LSFT_T(KC_MINS), KC_Z,  KC_X,    KC_C,            KC_V,  KC_B,           KC_N,           KC_M,    KC_COMM,        KC_DOT,  KC_SLSH, RSFT_T(KC_QUOT), \
-      KC_LCTL,         KC_NO, KC_LALT, LGUI_T(KC_BSPC), MO(1), LSFT_T(KC_SPC), LSFT_T(KC_SPC), MO(2),   RGUI_T(KC_ENT), KC_RALT, KC_NO,   KC_RCTL
+      KC_LCTL,         KC_NO, KC_LALT, LGUI_T(KC_BSPC), MO(1), LSFT_T(KC_SPC), LSFT_T(KC_SPC), MO(2),   KC_ENT,         KC_RGUI, KC_RALT,   KC_RCTL
     ),
     /* Symbol
      * ,-----------------------------------------------------------------------------------.
