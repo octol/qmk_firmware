04aea966c873ddd1da00cec7085ce095d3df10b1
Jon Häggblad <jon.haggblad@gmail.com>
2020-05-06 11:08:26 +0200

    set alt gui swap correctly

--- keyboards/preonic/keymaps/octol/keymap.c
+++ keyboards/preonic/keymaps/octol/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * |Shift |  Z   |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * | Ctrl |      | Alt  | GUI  |Symbol| Shift/Space |Enter | Nav  |  GUI | Alt  | Ctrl |
+     * | Ctrl |      | GUI  |Alt/Bp|Symbol| Shift/Space |Enter | Nav  |  Alt | GUI  | Ctrl |
      * `-----------------------------------------------------------------------------------'
      */
 	[_QWERTY] = LAYOUT_preonic_grid( \
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,          KC_Q,  KC_W,    KC_E,            KC_R,  KC_T,           KC_Y,           KC_U,    KC_I,           KC_O,    KC_P,    KC_BSPC, \
       LCTL_T(KC_ESC),  KC_A,  KC_S,    KC_D,            KC_F,  KC_G,           KC_H,           KC_J,    KC_K,           KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT,         KC_Z,  KC_X,    KC_C,            KC_V,  KC_B,           KC_N,           KC_M,    KC_COMM,        KC_DOT,  KC_SLSH, KC_RSFT, \
-      KC_LCTL,         KC_NO, KC_LALT, LGUI_T(KC_BSPC), MO(1), LSFT_T(KC_SPC), LSFT_T(KC_SPC), KC_ENT,  MO(2),          KC_RGUI, KC_RALT, KC_RCTL
+      KC_LCTL,         KC_NO, KC_LGUI, LALT_T(KC_BSPC), MO(1), LSFT_T(KC_SPC), LSFT_T(KC_SPC), KC_ENT,  MO(2),          KC_RALT, KC_RGUI, KC_RCTL
     ),
     /* Symbol
      * ,-----------------------------------------------------------------------------------.
