d8c68a83dfee509f92d077f9214856ac8dbc83ae
Jon Häggblad <jon.haggblad@gmail.com>
2020-05-27 10:50:56 +0200

    Add shift next to space

--- keyboards/preonic/keymaps/octol/keymap.c
+++ keyboards/preonic/keymaps/octol/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * |Shift |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * | Ctrl |      | GUI  | Alt  |Lower |    Space    |Raise | Alt  | GUI  |      | Ctrl |
+     * | Ctrl |      | GUI  | Alt  |Lower |    Space    |Raise |Shift | Alt  | GUI  | Ctrl |
      * `-----------------------------------------------------------------------------------'
      */
 	[_QWERTY] = LAYOUT_preonic_grid( \
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,         KC_Q,  KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC, \
       LCTL_T(KC_ESC), KC_A,  KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT, \
       KC_LSFT,        KC_Z,  KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-      KC_LCTL,        KC_NO, KC_LGUI, KC_LALT, MO(1), KC_SPC, KC_SPC, MO(2), KC_RALT, KC_RGUI, KC_NO,   KC_RCTL
+      KC_LCTL,        KC_NO, KC_LGUI, KC_LALT, MO(1), KC_SPC, KC_SPC, MO(2), KC_LSFT, KC_RALT, KC_RGUI, KC_RCTL
       ),
     /* Lower
      * ,-----------------------------------------------------------------------------------.
