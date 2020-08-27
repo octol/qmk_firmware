d184c6cbc97966b228d970a9ec7c4ad23212594e
Jon Häggblad <jon.haggblad@gmail.com>
2020-08-27 23:01:52 +0200

    Enable mouse buttons

--- keyboards/lily58/keymaps/octol/keymap.c
+++ keyboards/lily58/keymaps/octol/keymap.c
@@ -17,6 +17,10 @@ enum layer_number {
   _ADJUST,
 };
 
+
+// Ideas:
+//  - backspace on one of the index finger buttons.
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
