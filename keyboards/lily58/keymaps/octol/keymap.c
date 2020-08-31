3e7bd8797335d9f3de51d1c147baa3c0162b9bde
Jon Häggblad <jon.haggblad@gmail.com>
2020-08-31 12:30:35 +0200

    Move backspace to right

--- keyboards/lily58/keymaps/octol/keymap.c
+++ keyboards/lily58/keymaps/octol/keymap.c
@@ -108,10 +108,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 /*const uint16_t PROGMEM bspc_combo[] = {KC_H, KC_J, COMBO_END};*/
-const uint16_t PROGMEM lbspc_combo[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM bspc_combo[] = {KC_U, KC_I, COMBO_END};
 combo_t key_combos[COMBO_COUNT] = {
     /*COMBO(bspc_combo, KC_BSPC),*/
-    COMBO(lbspc_combo, KC_BSPC)
+    COMBO(bspc_combo, KC_BSPC)
 };
 
 // Setting ADJUST layer RGB back to default
