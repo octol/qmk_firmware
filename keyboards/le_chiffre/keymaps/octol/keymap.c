5c57b3208e363f2db1f342cc97a96ebc02f35c2e
Jon Häggblad <jon.haggblad@gmail.com>
2020-10-03 23:50:25 +0200

    Update

--- keyboards/le_chiffre/keymaps/octol/keymap.c
+++ keyboards/le_chiffre/keymaps/octol/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |SHFT/Z|  X  |  C  |  V  |  B  |      |  N  |  M  |  <  |  >  | SHFT/? |
  * `------+-----+-----+------+----'      `--------------------------------'
  *            .----------------.           .-------------.
- *            |ENT/ALT|SPC(SYM)|           |SPC(NUM)|BSPC|
+ *            |ENT/ALT|SPC(SYM)|           |SPC(NUM)|SHFT|
  *            '----------------'           '-------------'
  */
 
