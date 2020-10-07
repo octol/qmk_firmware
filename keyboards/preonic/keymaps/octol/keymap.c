47e2d82c512010225615ef2a4e172de6ed50f792
Jon Häggblad <jon.haggblad@gmail.com>
2020-10-07 10:58:37 +0200

    Put back ctrl on capslock and shift function keys

--- keyboards/preonic/keymaps/octol/keymap.c
+++ keyboards/preonic/keymaps/octol/keymap.c
@@ -35,11 +35,11 @@ enum preonic_keycodes {
  * ,-----------------------------------------------------------------------------------.
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |     ESC     |      |      |      |     ESC     |    BSLSH    |      |
+ * |      |      |     ESC     |      |      |      |     ESC     |     BSLS    |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |     BKSp   TAB     |      |     LESS   COLN  GREAT    |      |      |
+ * |      |      |     BSPC   TAB     |      |     LT     COLN   GT      |      |      |
  * |------+------+------+------+------+------|-ENT--+------+------+------+------+------|
- * |      |      |     DASH   ENTER   |      |     QUOT   UNDS    |      |      |      |
+ * |      |      |     MINS   ENTER   |      |     QUOT   UNDS    |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
@@ -60,12 +60,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      */
 	[_QWERTY] = LAYOUT_preonic_grid( \
-      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS, \
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC, \
-      KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-      KC_LCTL, _______, KC_LGUI, KC_LALT, MO(1), KC_SPC, KC_SPC, MO(2), KC_RALT, KC_RGUI, _______, KC_RCTL
-      ),
+      KC_ESC,         KC_1,    KC_2,    KC_3,    KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS, \
+      KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC, \
+      LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+      KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+      KC_LCTL,        _______, KC_LGUI, KC_LALT, MO(1), KC_SPC, KC_SPC, MO(2), KC_RALT, KC_RGUI, _______, KC_RCTL
+    ),
     /* Lower
      * ,-----------------------------------------------------------------------------------.
      * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  _   |
@@ -108,9 +108,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     /* Adjust (Lower + Raise)
      * ,-----------------------------------------------------------------------------------.
-     * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+     * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      | Reset|Debug |      |      |      |      |TermOn|TermOf|      |      |      |
+     * |      | Reset|Debug |      |      |      |      |TermOn|TermOf|      |      |  F12 |
      * |------+------+------+------+------+-------------+------+------+------+------+------|
      * |      |      |      |Aud on|AudOff|AGnorm|AGswap|RGBtog|      |      |      |      |
      * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -120,8 +120,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      */
 	[_ADJUST] = LAYOUT_preonic_grid( \
-      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12, \
-      _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF, _______, _______, _______, \
+      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11, \
+      _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF, _______, _______, KC_F12, \
       _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, RGB_TOG, _______,  _______, _______, _______, \
       _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______,  _______, _______, _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______ \
@@ -165,8 +165,8 @@ combo_t key_combos[COMBO_COUNT] = {
   //COMBO(combo_del, KC_DEL),
   COMBO(combo_dash, KC_MINS),
   COMBO(combo_ent, KC_ENT),
-  COMBO(combo_esc2, KC_ESC),
 
+  COMBO(combo_esc2, KC_ESC),
   COMBO(combo_bksl, KC_BSLS),
   COMBO(combo_ent2, KC_ENT),
   COMBO(combo_less, KC_LT),
