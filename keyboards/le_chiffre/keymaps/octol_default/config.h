153c3ccb3971ea85355be3d77edeae96a9a3ab4e
Jon Häggblad <jon.haggblad@gmail.com>
2020-10-07 14:20:19 +0200

    Enable OLED in octol_default

--- keyboards/le_chiffre/keymaps/octol_default/config.h
+++ keyboards/le_chiffre/keymaps/octol_default/config.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #define TAPPING_TERM 200
-#define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
-#define TAPPING_FORCE_HOLD
+//#define PERMISSIVE_HOLD
+//#define TAPPING_FORCE_HOLD
+//#define RETRO_TAPPING
 
 #define COMBO_COUNT 1
-#define COMBO_TERM 40
+#define COMBO_TERM 30
 
 //#define FORCE_NKRO
 //#define EXTRA_EXTRA_LONG_COMBOS
--- keyboards/le_chiffre/keymaps/octol_default/rules.mk
+++ keyboards/le_chiffre/keymaps/octol_default/rules.mk
@@ -1,1 +1,2 @@
 COMBO_ENABLE = yes
+OLED_DRIVER_ENABLE = yes
