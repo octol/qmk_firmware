77d8713c56d20647cfbfc0b537d862dc553686d9
Jon Häggblad <jon.haggblad@gmail.com>
2020-10-29 23:39:16 +0100

    Setup my standard layout

--- keyboards/ergotravel/keymaps/octol/keymap.c
+++ keyboards/ergotravel/keymaps/octol/keymap.c
@@ -16,42 +16,44 @@ enum custom_keycodes {
 #define CALTDEL LCTL(LALT(KC_DEL))
 #define TSKMGR LCTL(LSFT(KC_ESC))
 
+#define CTL_ESC LCTL_T(KC_ESC)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //,--------+--------+--------+--------+--------+--------+--------.        ,--------+--------+--------+--------+--------+--------+--------.
-     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,          KC_EQL, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    RESET,            _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,          KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+     CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______,          _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,           KC_SPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,          _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     KC_LCTL, KC_LGUI, KC_LALT, ADJUST,           LOWER,   KC_SPC,           KC_SPC,  RAISE,            KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
+     KC_LCTL, _______, KC_LGUI, KC_LALT,          LOWER,   KC_SPC,           KC_ENT,  RAISE,            KC_RALT, KC_RGUI, _______, KC_RCTL
   //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
   ),
 
   [_LOWER] = LAYOUT(
-  KC_TILD, KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_HOME,          KC_PGUP,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
-  _______, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_END ,          KC_PGDN,     KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_BSLS, \
-  _______, KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_BSPC,          KC_BSPC,     KC_F12,  _______, _______, KC_MUTE, _______, KC_PIPE, \
-  _______, _______,  _______, _______, _______, KC_BSPC,                                KC_BSPC, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
+  KC_TILD, KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,          _______,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
+  _______, KC_LCBR,  KC_RCBR, KC_LPRN, KC_RPRN, KC_PIPE, _______,          _______,     KC_PLUS, KC_MINS, KC_UNDS, KC_EQL,  KC_COLN, KC_DQUO, \
+  _______, _______,  _______, KC_LBRC, KC_RBRC, KC_BSLS, _______,          _______,     KC_COLN, KC_QUOT, KC_DQUO, _______, _______, _______, \
+  _______, _______,  _______, _______,          _______, _______,          _______, _______,              _______, _______, _______, _______  \
 
   ),
 
   [_RAISE] = LAYOUT(
- 
-  KC_ESC,  KC_1,    KC_2,  KC_3,   KC_4,    KC_5,     _______,          _______,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
-  _______, KC_4,    KC_5,  KC_6,   KC_PLUS, _______,  _______,          _______,   _______,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______, \
-  KC_ENT,  KC_7,    KC_8,  KC_9,   KC_MINS, _______,  _______,          _______,   _______,   KC_NUHS, KC_NUBS, KC_MUTE, _______, KC_BSLS, \
-  _______, KC_COMM, KC_0,  KC_DOT, _______, KC_BSPC,                                 KC_BSPC, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
-  
+
+  KC_GRV,  KC_1,    KC_2,   KC_3,   KC_4,    KC_5,     _______,          _______,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_INS,  \
+  _______, _______, KC_BTN1,KC_MS_U,KC_BTN2, _______,  _______,          _______,   KC_LEFT,   KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
+  _______, _______, KC_MS_L,KC_MS_D,KC_MS_R, _______,  _______,          _______,   KC_HOME,   KC_PGUP, KC_PGDN, KC_END,  _______, _______, \
+  _______, _______, _______,_______,         _______,  _______,          _______,   _______,            _______, _______, _______, _______  \
+
   ),
 
   [_ADJUST] = LAYOUT(
-     TSKMGR,  _______,  _______, _______, _______, _______, _______,         _______,  _______, RGB_MOD, RGB_VAI, RGB_SAI, RGB_HUI, CALTDEL,
-     _______, _______,  _______, _______, _______, _______, _______,         _______,  _______, RGB_RMOD, RGB_VAD, RGB_SAD, RGB_HUD, RGB_TOG,
-     _______, _______,  _______, _______, _______, _______, _______,         _______,  _______, _______, _______, _______, _______, BL_STEP,
-     _______, _______, _______, _______,           _______, _______,         _______,  _______,          _______, _______, _______, RESET
+     _______, _______, _______, _______, _______, _______, _______,         _______,  _______, _______, _______, _______, _______, _______,
+     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,         _______,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+     _______, _______, _______, _______, _______, _______, _______,         _______,  _______, _______, _______, KC_F11,  KC_F12,  _______,
+     _______, _______, _______, _______,          _______, _______,         _______,  _______,          _______, _______, _______, _______
   )
 
 };
