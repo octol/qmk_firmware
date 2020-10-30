8ec6c0479332cd0652e9a0c16fcb4f32c5ffc7d5
Jon Häggblad <jon.haggblad@gmail.com>
2020-10-30 21:38:17 +0100

    Update modtap settings on lechiffre

--- keyboards/le_chiffre/keymaps/octol/keymap.c
+++ keyboards/le_chiffre/keymaps/octol/keymap.c
@@ -138,8 +138,7 @@ combo_t key_combos[COMBO_COUNT] = {
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LCTL_T(KC_A):
-            //return 350; // used with no TAPPING_TERM
-            return 250; // used with TAPPING_TERM
+            return 300;
         default:
             return TAPPING_TERM;
     }
