626d0aad4cc5feed28f4d461e9372eac24777e21
Jon Häggblad <jon.haggblad@gmail.com>
2020-10-30 15:01:15 +0100

    Tweak LeChiffre layout

--- keyboards/le_chiffre/keymaps/octol_gergoplex/keymap.c
+++ keyboards/le_chiffre/keymaps/octol_gergoplex/keymap.c
@@ -68,14 +68,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  %   |  ^  |  [  |  ]  | RMB |      |  &  |  =  |  ,  |  .  |   -    |
  * `------+-----+-----+------+----'      `--------------------------------'
  *                .---------------.      .---------------.
- *                |  ;   |        |      |    =    |  ;  |
+ *                |      |        |      |        | RALT |
  *                '---------------'      '---------------'
  */
 [_SYMB] = LAYOUT(
   KC_EXLM, KC_AT,    KC_LCBR, KC_RCBR, KC_PIPE, _______, KC_GRV,  KC_TILD, _______, _______, KC_BSLS,
   KC_HASH, KC_DLR,   KC_LPRN, KC_RPRN, KC_BTN2,          KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_QUOT,
   KC_PERC, KC_CIRC,  KC_LBRC, KC_RBRC, KC_BTN1,          KC_AMPR, KC_EQL,  KC_COMM, KC_DOT,  KC_MINS,
-                              KC_SCLN, _______,          KC_EQL,  KC_SCLN
+                              _______, _______,          _______,  KC_RALT
 ),
 
 /* Keymap 2: Pad/Function layer
@@ -92,8 +92,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_NUMB] = LAYOUT(
   KC_1,   KC_2,  KC_3,  KC_4,  KC_5, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-  KC_F1,  KC_F2, KC_F3, KC_F4, KC_F5,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLU,
-  KC_F6,  KC_F7, KC_F8, KC_F9, KC_F10,        KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_VOLD,
+  KC_F1,  KC_F2, KC_F3, KC_F4, KC_F5,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+  KC_F6,  KC_F7, KC_F8, KC_F9, KC_F10,        KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______,
                         KC_F11, KC_F12,       _______, RESET
 ),
 
