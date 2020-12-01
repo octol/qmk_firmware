11632e98a72a34d5bbacf89b5ce4b954c1c25c90
Jon Häggblad <jon.haggblad@gmail.com>
2020-12-01 16:42:08 +0100

    Update ergotravel to latest

--- keyboards/ergotravel/keymaps/octol/keymap.c
+++ keyboards/ergotravel/keymaps/octol/keymap.c
@@ -16,9 +16,15 @@ enum custom_keycodes {
 #define CALTDEL LCTL(LALT(KC_DEL))
 #define TSKMGR LCTL(LSFT(KC_ESC))
 
-#define CTL_ESC LCTL_T(KC_ESC)
 #define PREV_TAB LCTL(KC_PGUP)
 #define NEXT_TAB LCTL(KC_PGDN)
+
+#define LCTL_ESC LCTL_T(KC_ESC)
+#define RCTL_QUOT RCTL_T(KC_QUOT)
+
+#define SV_AA RALT(KC_W)
+#define SV_AE RALT(KC_Q)
+#define SV_OE RALT(KC_P)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -26,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------+--------+--------+--------+--------+--------+--------.        ,--------+--------+--------+--------+--------+--------+--------.
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    RESET,            _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______,          _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+     LCTL_ESC,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______,          _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RCTL_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,          _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
@@ -36,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT(
   KC_TILD, KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,          _______,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
-  _______, KC_LCBR,  KC_RCBR, KC_LPRN, KC_RPRN, KC_PIPE, _______,          _______,     KC_PLUS, KC_MINS, KC_UNDS, KC_EQL,  _______, PREV_TAB, \
+  KC_ESC,  KC_LCBR,  KC_RCBR, KC_LPRN, KC_RPRN, KC_PIPE, _______,          _______,     KC_PLUS, KC_MINS, KC_UNDS, KC_EQL,  _______, PREV_TAB, \
   _______, _______,  _______, KC_LBRC, KC_RBRC, KC_BSLS, _______,          _______,     KC_COLN, KC_QUOT, KC_DQUO, _______, _______, NEXT_TAB, \
   _______, _______,  _______, _______,          _______, _______,          _______, _______,              _______, _______, _______, _______  \
 
@@ -45,15 +51,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RAISE] = LAYOUT(
 
   KC_GRV,  KC_1,    KC_2,   KC_3,   KC_4,    KC_5,     _______,          _______,   KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_INS,  \
-  _______, _______, KC_BTN1,KC_MS_U,KC_BTN2, PREV_TAB, _______,          _______,   KC_LEFT,   KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
-  _______, _______, KC_MS_L,KC_MS_D,KC_MS_R, NEXT_TAB, _______,          _______,   KC_HOME,   KC_PGUP, KC_PGDN, KC_END,  _______, _______, \
+  _______, KC_LGUI, KC_LALT,KC_LCTL,KC_LSFT, PREV_TAB, _______,          _______,   KC_LEFT,   KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
+  _______, _______, SV_AA,  SV_AE,  SV_OE,   NEXT_TAB, _______,          _______,   KC_HOME,   KC_PGUP, KC_PGDN, KC_END,  _______, _______, \
   _______, _______, _______,_______,         _______,  _______,          _______,   _______,            _______, _______, _______, _______  \
 
   ),
 
   [_ADJUST] = LAYOUT(
      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,         _______,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-     _______, _______, _______, _______, _______, _______, _______,         _______,  _______, _______, _______, KC_F11,  KC_F12,  _______,
+     _______, _______, _______, _______, _______, RESET,   _______,         _______,  _______, _______, _______, KC_F11,  KC_F12,  _______,
      _______, _______, _______, _______, _______, _______, _______,         _______,  _______, _______, _______, _______, _______, _______,
      _______, _______, _______, _______,          _______, _______,         _______,  _______,          _______, _______, _______, _______
   )
@@ -70,12 +76,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
 //|
 //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
-const uint16_t PROGMEM combo_esc[]  = {KC_W, KC_E, COMBO_END};
+const uint16_t PROGMEM combo_esc[]  = {KC_W, KC_R, COMBO_END};
 const uint16_t PROGMEM combo_del[]  = {KC_A, KC_S, COMBO_END};
 const uint16_t PROGMEM combo_bspc[] = {KC_S, KC_D, COMBO_END};
 const uint16_t PROGMEM combo_tab[]  = {KC_D, KC_F, COMBO_END};
 const uint16_t PROGMEM combo_mins[] = {KC_X, KC_C, COMBO_END};
-const uint16_t PROGMEM combo_ent[]  = {KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM combo_ent[]  = {KC_X, KC_V, COMBO_END};
+
+/*const uint16_t PROGMEM combo_prev_tab[]  = {KC_F, KC_G, COMBO_END};*/
+/*const uint16_t PROGMEM combo_next_tab[]  = {KC_V, KC_B, COMBO_END};*/
 
 const uint16_t PROGMEM combo_esc2[] = {KC_U, KC_I, COMBO_END};
 const uint16_t PROGMEM combo_bsls[] = {KC_O, KC_P, COMBO_END};
@@ -93,6 +102,9 @@ combo_t key_combos[COMBO_COUNT] = {
   COMBO(combo_tab, KC_TAB),
   COMBO(combo_mins, KC_MINS),
   COMBO(combo_ent, KC_ENT),
+
+  /*COMBO(combo_prev_tab, PREV_TAB),*/
+  /*COMBO(combo_next_tab, NEXT_TAB),*/
 
   COMBO(combo_esc2, KC_ESC),
   COMBO(combo_bsls, KC_BSLS),
