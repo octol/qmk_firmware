dd95eaf4d1a8686b4977ece2bbfebbc5dda0d676
Jon Häggblad <jon.haggblad@gmail.com>
2021-02-17 10:47:06 +0100

    Switch to numpad layout for boardwalk

--- keyboards/boardwalk/keymaps/octol/keymap.c
+++ keyboards/boardwalk/keymaps/octol/keymap.c
@@ -21,6 +21,7 @@ enum layer_names {
     _SYMB,
     _NUM,
     _ADJUST,
+    _NUMPAD,
 };
 
 #define PREV_TAB LCTL(KC_PGUP)
@@ -28,6 +29,8 @@ enum layer_names {
 
 #define RCTL_QUOT RCTL_T(KC_QUOT)
 #define RALT_DOT  RALT_T(KC_DOT)
+
+#define NUM_TAB LT(_NUMPAD, KC_TAB)
 
 #define SV_AA RALT(KC_W)
 #define SV_AE RALT(KC_Q)
@@ -50,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_BASE] = LAYOUT_ortho_hhkb(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_HOME, KC_PGUP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        NUM_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_HOME, KC_PGUP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RCTL_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                  KC_LCTL, KC_LGUI, KC_LALT, MO(_SYMB),        KC_SPC,  KC_ENT,           MO(_NUM),KC_RALT, KC_RGUI, KC_RCTRL
@@ -66,9 +69,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NUM] = LAYOUT_ortho_hhkb(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_INS,
-        _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-        _______, _______, SV_AA,   SV_AE,   SV_OE,   _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______, _______,
+        KC_GRV,  SV_AA,   SV_AE,   KC_7,    KC_8,    KC_9,    _______, _______, _______, _______, _______, _______, SV_OE,   KC_INS,
+        _______, KC_PLUS, KC_MINS, KC_4,    KC_5,    KC_6,    _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+        _______, KC_ASTR, KC_0,    KC_1,    KC_2,    KC_3,    _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______, _______,
                  _______, _______, _______, _______,          _______, _______,          _______, _______, _______, _______
     ),
 
@@ -76,7 +79,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
         _______, RGB_TOG, KC_APP,  _______, _______, RESET,   _______, _______, _______, _______, _______, KC_F11,  KC_F12,  _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_MUTE, KC_VOLD, KC_VOLU, KC_BSLS, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,
+                 _______, _______, _______, _______,          _______, _______,          _______, _______, _______, _______
+    ),
+
+    [_NUMPAD] = LAYOUT_ortho_hhkb(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_ASTR, KC_7,    KC_8,    KC_9,    KC_MINS,_______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_SLSH, KC_4,    KC_5,    KC_6,    KC_PLUS, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_0,    KC_1,    KC_2,    KC_3,    _______, _______,
                  _______, _______, _______, _______,          _______, _______,          _______, _______, _______, _______
     )
 };
